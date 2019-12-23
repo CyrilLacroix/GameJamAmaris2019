e5f13b8cc69599c917eaf61e45a1a843c94d32c7
CyrilLacroix <cyril.lacroix@outlook.com>
2019-12-23 19:35:18 +0100

    Add Spec for Tests

---
+++ Architecture/spec/main_spec.lua
@@ -0,0 +1,11 @@
+
+-- local love = {
+--     main = require 'main'
+-- }
+
+describe('main', function()
+  it('builds', function()
+    -- main.load()
+    -- assert.equal(5, mylib.add(2,3))
+  end)
+end)
